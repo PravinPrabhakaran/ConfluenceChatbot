67ab4eddf1ec58dfca2d0bfa28cdba45635f9a4d
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-07-20 13:52:22 +0100

    css changes

--- my-app/src/Chatbox.js
+++ my-app/src/Chatbox.js
@@ -5,10 +5,13 @@ import { Container, Card, Button, Form } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Chatbox() {
+function Chatbox(props) {
+
+    const borderRadiusStyle = props.bot ? { borderTopLeftRadius: '0'} : { borderTopRightRadius: '0' };
+    const alignSelfStyle = props.bot ? { alignSelf: 'start' } : { alignSelf: 'end' };
 
   return (
-    <Card className="chat-box rounded-lg shadow-l align-self-start">
+    <Card className="chat-box rounded-lg shadow-l" style={{...borderRadiusStyle, ...alignSelfStyle}}>
         <Card.Body>
         <div className="message">
             <p>Hello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?ello, how can I help you?asdasdaasdjasidjsaidjisajidiasdijajid saidjisajidiasdijajid saidjisajidiasdijajid saidjisajidiasdijajid saidjisajidiasdijajid saidjisajidiasdijajid</p>
